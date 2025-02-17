da31dd3ec0f7fd645f862b7c594c20566f9b1619
eliza2526 <elizabeth.thomas2@georgebrown.ca>
2025-02-16 19:56:45 -0500

    "Apollo, GraphQL Exercise"

--- index.js
+++ index.js
@@ -1,45 +1,55 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const schema = require('./schema');
+const resolver = require('./resolvers');
+
 //import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
 
-
-//Store sensitive information to env variables
+//store sensitive information to env variables
 const dotenv = require('dotenv');
 dotenv.config();
 
 //mongoDB Atlas Connection String
-const mongodb_atlas_url = process.env.MONGODB_URL;
+const DB_CONNECTION = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.${process.env.CLUSTER_ID}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
+const mongodb_atlas_url = process.env.MONGODB_URL || DB_CONNECTION;
 
-//TODO - Replace you Connection String here
-const connectDB = async() => {
-    try{
-      mongoose.connect(mongodb_atlas_url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      }).then(success => {
-        console.log('Success Mongodb connection')
-      }).catch(err => {
-        console.log('Error Mongodb connection')
-      });
-    } catch(error) {
-        console.log(`Unable to connect to DB : ${error.message}`);
-      }
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongodb_atlas_url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Success Mongodb connection');
+  } catch (error) {
+    console.log(`Unable to connect to DB : ${error.message}`);
   }
+};
 
 //Define Apollo Server
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers: resolver,
+  });
 
+  
+  await server.start(); 
 
-//Define Express Server
-const app = express();
-app.use(express.json());
-app.use('*', cors());
+  //Define Express Server
+  const app = express();
+  app.use(express.json());
+  app.use('*', cors());
 
-//Add Express app as middleware to Apollo Server
+  //Add Express app as middleware to Apollo Server
+  server.applyMiddleware({ app });
 
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+    connectDB();
+  });
+};
 
-//Start listen 
-app.listen({ port: process.env.PORT }, () => {  
-  console.log(`🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`)
-  connectDB()
-});
+startServer();
--- resolvers.js
+++ resolvers.js
@@ -1,1 +1,64 @@
 const Movie = require('./models/Movie');
+
+const resolvers = {
+  Query: {
+    
+    movies: async () => {
+      try {
+        return await Movie.find();
+      } catch (err) {
+        throw new Error('Error fetching movies');
+      }
+    },
+    
+    movie: async (_, { id }) => {
+      try {
+        return await Movie.findById(id);
+      } catch (err) {
+        throw new Error('Error fetching movie');
+      }
+    },
+  },
+  
+  Mutation: {
+    
+    addMovie: async (_, { name, director_name, production_house, release_date, rating }) => {
+      try {
+        const newMovie = new Movie({
+          name,
+          director_name,
+          production_house,
+          release_date,
+          rating,
+        });
+        return await newMovie.save();
+      } catch (err) {
+        throw new Error('Error adding movie');
+      }
+    },
+    
+    updateMovie: async (_, { id, name, director_name, production_house, release_date, rating }) => {
+      try {
+        const updatedMovie = await Movie.findByIdAndUpdate(
+          id,
+          { name, director_name, production_house, release_date, rating },
+          { new: true }
+        );
+        return updatedMovie;
+      } catch (err) {
+        throw new Error('Error updating movie');
+      }
+    },
+    
+    deleteMovie: async (_, { id }) => {
+      try {
+        const deletedMovie = await Movie.findByIdAndRemove(id);
+        return deletedMovie;
+      } catch (err) {
+        throw new Error('Error deleting movie');
+      }
+    },
+  },
+};
+
+module.exports = resolvers;
